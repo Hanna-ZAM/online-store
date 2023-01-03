7b1e0f668e84347dd26c99637793cca07d84157d
Andrei Kanoplich <andrei.kanoplich@gmail.com>
2023-01-03 23:26:39 +0300

    feat: add error page

--- src/pages/error/error.ts
+++ src/pages/error/error.ts
@@ -1,4 +1,6 @@
+import './error.css';
 import Page from '../../core/templates/page';
+import App from '../app/app';
 
 export enum ErrorTypes {
   Error_404 = 404,
@@ -8,7 +10,7 @@ class ErrorPage extends Page {
   private errorType: ErrorTypes | string;
 
   static TextObject: { [prop: string]: string } = {
-    '404': 'Страница не найдена',
+    '404': 'Page not found',
   };
 
   constructor(id: string, errorType: ErrorTypes | string) {
@@ -17,8 +19,21 @@ class ErrorPage extends Page {
   }
 
   render() {
-    const title = this.createTitle(ErrorPage.TextObject[this.errorType]);
-    this.container.append(title);
+    // const title = this.createTitle(ErrorPage.TextObject[this.errorType]);
+    const title = this.createElement('', 'div', 'error__container')
+    this.container.appendChild(title);
+
+    const errorPage = this.createElement('Page not found', 'span', 'error__page');
+    const errorNumber = this.createElement('Sorry, but the page you were looking for could not be found.', 'span', 'error__number');
+    const errorBtn = this.createElement('Homepage', 'div', 'error__btn');
+    // errorBtn.setAttribute('href', '/#main');
+    errorBtn.addEventListener('click', () => {
+      history.pushState({}, '', `/main`);
+      App.renderNewPage('main');
+    })
+    title.append(errorPage);
+    title.append(errorNumber);
+    title.append(errorBtn);
     return this.container;
   }
 }
